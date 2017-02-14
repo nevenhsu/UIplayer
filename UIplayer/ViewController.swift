0f857d8bff5dda30ab8155b995728d47b2ced288
Neven <neven@Nevens-MacBook-Pro.local>
2017-02-14 21:21:24 +0800

    Initial Commit

---
+++ UIplayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIplayer
+//
+//  Created by Neven on 14/02/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
