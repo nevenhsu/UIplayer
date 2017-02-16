0309af8840fc394d7fbc67e4e1f49568861af73e
Neven <neven@Nevens-MacBook-Pro.local>
2017-02-16 23:47:57 +0800

    set up cell UI

---
+++ UIplayer/ItemCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCell.swift
+//  UIplayer
+//
+//  Created by Neven on 16/02/2017.
+//  Copyright © 2017 Neven. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
